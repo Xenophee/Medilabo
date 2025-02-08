24f0fcea581024133022a6515e20408aaf8bca47
Perrine Dassonville <perrine.dassonville@outlook.fr>
2025-02-08 20:13:31 +0100

    Correctif de la BDD

--- patient-service/src/resources/bdd.sql
+++ patient-service/src/resources/bdd.sql
@@ -5,8 +5,8 @@ SET search_path TO dev;
 
 CREATE TABLE Patients (
       id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-      first_name VARCHAR(70) NOT NULL,
       last_name VARCHAR(70) NOT NULL,
+      first_name VARCHAR(70) NOT NULL,
       birthdate DATE NOT NULL,
       gender CHAR(1) NOT NULL CHECK (gender IN ('M', 'F')),
       address VARCHAR(150),
@@ -14,7 +14,7 @@ CREATE TABLE Patients (
 );
 
 
-INSERT INTO Patients (first_name, last_name, birthdate, gender, address, phone)
+INSERT INTO Patients (last_name, first_name, birthdate, gender, address, phone)
 VALUES ('TestNone', 'Test', '1966-12-31', 'F', '1 Brookside St', '100-222-3333'),
        ('TestBorderline', 'Test', '1945-06-24', 'M', '2 High St', '200-333-4444'),
        ('TestInDanger', 'Test', '2004-06-18', 'M', '3 Club Road', '300-444-5555'),
