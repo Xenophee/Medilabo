21f4e88820f578909e417755eafb799bff245155
Perrine Dassonville <perrine.dassonville@outlook.fr>
2025-02-08 20:15:29 +0100

    Ajout de feign

--- client-service/src/main/java/com/medilabo/clientservice/ClientServiceApplication.java
+++ client-service/src/main/java/com/medilabo/clientservice/ClientServiceApplication.java
@@ -2,8 +2,11 @@ package com.medilabo.clientservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
+@EnableFeignClients
 public class ClientServiceApplication {
 
     public static void main(String[] args) {
