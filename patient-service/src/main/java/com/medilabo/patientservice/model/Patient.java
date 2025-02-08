ce67664d2d3d828fe90e7d5db498e6f3117bfbee
Perrine Dassonville <perrine.dassonville@outlook.fr>
2025-02-08 20:16:18 +0100

    Mise à jour du model avec les messages de validation

--- patient-service/src/main/java/com/medilabo/patientservice/model/Patient.java
+++ patient-service/src/main/java/com/medilabo/patientservice/model/Patient.java
@@ -1,5 +1,7 @@
 package com.medilabo.patientservice.model;
 
+import com.medilabo.patientservice.validation.annotation.ValidAddress;
+import com.medilabo.patientservice.validation.annotation.ValidPhone;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.Getter;
@@ -20,25 +22,26 @@ public class Patient {
     private UUID id;
 
     @Column(name = "last_name")
-    @NotBlank
-    @Size(min = 2, max = 70)
+    @NotBlank(message = "Le renseignement du nom est obligatoire.")
+    @Size(min = 2, max = 70, message = "Le nom doit contenir entre 2 et 70 caractères.")
     private String lastName;
 
     @Column(name = "first_name")
-    @NotBlank
-    @Size(min = 2, max = 70)
+    @NotBlank(message = "Le renseignement du prénom est obligatoire.")
+    @Size(min = 2, max = 70, message = "Le prénom doit contenir entre 2 et 70 caractères.")
     private String firstName;
 
-    @NotNull
+    @NotNull(message = "Le renseignement de la date de naissance est obligatoire.")
+    @PastOrPresent(message = "La date de naissance ne peut pas être dans le futur.")
     private LocalDate birthdate;
 
-    @NotBlank
-    @Pattern(regexp = "^[FM]$")
+    @NotBlank(message = "Le renseignement du genre est obligatoire.")
+    @Pattern(regexp = "^[FM]$", message = "Le genre doit être M ou F.")
     private String gender;
 
-    @Size(min = 5, max = 150)
+    @ValidAddress
     private String address;
 
-    @Size(min = 10, max = 15)
+    @ValidPhone
     private String phone;
 }
