ada0b4b7c447a1d48e47742c53afc27432f41087
Perrine Dassonville <perrine.dassonville@outlook.fr>
2025-02-09 14:25:55 +0100

    Ajout de la gestion d'erreurs en fonction des codes API

--- client-service/src/main/java/com/medilabo/clientservice/exception/GlobalExceptionHandler.java
+++ client-service/src/main/java/com/medilabo/clientservice/exception/GlobalExceptionHandler.java
@@ -4,14 +4,31 @@ package com.medilabo.clientservice.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public String handleResponseStatusException(ResponseStatusException ex, HttpServletRequest request, RedirectAttributes redirectAttributes, Model model) {
+        HttpStatus status = HttpStatus.valueOf(ex.getStatusCode().value());
+
+        if (status == HttpStatus.NOT_FOUND) return "views/404";
+
+        redirectAttributes.addFlashAttribute("errorMessage", ex.getReason());
+        String currentView = request.getRequestURI();
+        return "redirect:" + currentView;
+    }
+
 
     @ExceptionHandler(Exception.class)
     public String handleGeneralException(Exception ex, HttpServletRequest request) {
---
+++ client-service/src/main/java/com/medilabo/clientservice/feign/FeignClientErrorDecoder.java
@@ -0,0 +1,52 @@
+package com.medilabo.clientservice.feign;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.Response;
+import feign.Util;
+import feign.codec.ErrorDecoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+@Component
+public class FeignClientErrorDecoder implements ErrorDecoder {
+
+    private static final Logger logger = LoggerFactory.getLogger(FeignClientErrorDecoder.class);
+
+    private final ErrorDecoder defaultDecoder = new Default();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+
+    @Override
+    public Exception decode(String methodKey, Response response) {
+        String errorMessage = extractErrorMessage(response);
+
+        return switch (response.status()) {
+            case 404 -> new ResponseStatusException(HttpStatus.NOT_FOUND);
+            case 409 -> new ResponseStatusException(HttpStatus.CONFLICT, errorMessage);
+            default -> new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, "Erreur de communication avec le service distant.");
+        };
+    }
+
+
+    private String extractErrorMessage(Response response) {
+        try {
+            if (response.body() != null) {
+                String body = Util.toString(new InputStreamReader(response.body().asInputStream()));
+                Error errorResponse = objectMapper.readValue(body, Error.class);
+                return errorResponse.getMessage();
+            }
+        } catch (IOException e) {
+            logger.error("Erreur lors de la lecture du message d'erreur : ", e);
+        }
+        return "Erreur inattendue.";
+    }
+
+}
+
