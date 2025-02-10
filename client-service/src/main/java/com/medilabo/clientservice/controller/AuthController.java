2b251c4f73e372ef85c178a594f28833f34d8f0a
Perrine Dassonville <perrine.dassonville@outlook.fr>
2025-02-10 15:44:10 +0100

    Ajout d'un endpoint pour la 403

--- client-service/src/main/java/com/medilabo/clientservice/controller/AuthController.java
+++ client-service/src/main/java/com/medilabo/clientservice/controller/AuthController.java
@@ -1,8 +1,14 @@
 package com.medilabo.clientservice.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class AuthController {
+
+    @GetMapping("/access-denied")
+    public String accessDenied() {
+        return "/views/403";
+    }
 }
